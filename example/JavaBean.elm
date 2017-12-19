ea6c916338fa04266503ccd3f7d8e14376bf9d79
Rupert Smith <rupert@thesett.com>
2017-12-19 17:04:36 +0000

    Working on an example using the JavaBean template to create a model.

---
+++ example/JavaBean.elm
@@ -0,0 +1,78 @@
+module JavaBean exposing (..)
+
+import Html exposing (Html, div, text, pre)
+import JavaBuilder exposing (..)
+import JavaPrint exposing (javaSourceToString)
+
+
+main : Html Never
+main =
+    div []
+        [ pre [] [ text <| javaSourceToString javaExample ] ]
+
+
+javaExample : JavaSource
+javaExample =
+    file
+        [ package "com.analog.metermodel.model"
+        , imports
+            [ "java.io.Serializable"
+            , "com.fasterxml.jackson.annotation.JsonIgnoreProperties"
+            , "com.fasterxml.jackson.annotation.JsonInclude"
+            , "static com.fasterxml.jackson.annotation.JsonInclude.Include"
+            , "com.thesett.util.entity.Entity"
+            , "io.swagger.annotations.ApiModelProperty"
+            ]
+        ]
+        [ class "SomeEntity"
+            [ comment "Generated code from catalogue model."
+            , public
+            , implements [ "Entity<Long>", "Serializable" ]
+            , annotate
+                [ annotation "JsonIgnoreProperties" [ annotationNameValue "ignoreUnknown" "true" ]
+                , annotation "JsonInclude" [ annotationNameValue "" "Include.NON_NULL" ]
+                ]
+            ]
+            [ idField
+            , consNoArg
+            , getter "id" "Long"
+            , setter "id" "Long"
+            ]
+        ]
+
+
+idField =
+    field "Long"
+        "id"
+        [ comment "Holds the database surrogate id."
+        , private
+        ]
+
+
+consNoArg =
+    constructor
+        [ comment "No-arg constructor for serialization."
+        , public
+        ]
+        []
+
+
+getter field jtype =
+    method ("get" ++ field)
+        [ comment ("Gets the " ++ field)
+        , public
+        , returnType jtype
+        ]
+        [--return (var "id")
+        ]
+
+
+setter field jtype =
+    method ("set" ++ field)
+        [ comment ("Sets the " ++ field)
+        , public
+        , returnType "void"
+        , args [ arg ( jtype, field ) ]
+        ]
+        [ assign ("this." ++ field) (var field)
+        ]
